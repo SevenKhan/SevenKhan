fea0b43d19e1a4df3d11d88bfc6795a9c3ddd69c
SevenKhan <muammer345253@gmail.com>
2025-10-11 10:11:38 +0300

    Add mouse movement responsive animation

--- script.js
+++ script.js
@@ -56,3 +56,10 @@ button.addEventListener('click', (e) => {
     }, 1000);
   }
 });
+
+// Fare hareketine duyarlı animasyon
+document.addEventListener('mousemove', (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 20; // -10px ila +10px
+  const y = (e.clientY / window.innerHeight - 0.5) * 20;
+  document.querySelector('main').style.transform = `translate(${x}px, ${y}px)`;
+});
